f43b4d7e8726c74d920bb9eed8cf144a0d158288
Michael Whitten <michael.whitten@ni.com>
2023-03-08 10:28:00 -0600

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# rf-power-meter-measurement-link
+# RF Power Meter Measurement Link Service
+This code implements a basic power meter measurement using the NI [MeasurementLink](https://www.ni.com/en-us/shop/electronic-test-instrumentation/application-software-for-electronic-test-and-instrumentation-category/what-is-measurementlink.html) software. This adds support for interactive measurements inside of [InstrumentStudio](https://duckduckgo.com/?q=site%3Ani.com+instrumentstudio&ia=web) and as well as supporting automation use cases.
+
+![UI Example](/_images/UI.png)
+
+## Getting Started
+1) Ensure that you have installed the latest version of: 
+    - [InstrumentStudio](https://www.ni.com/en-us/support/downloads/software-products/download.instrumentstudio.html)
+    - [MeasurementLink](https://www.ni.com/en-us/support/downloads/software-products/download.measurementlink.html)
+    - [Python](https://www.python.org/downloads/)
+2) Run [start.bat](/RFPowerMeter/start.bat)
+3) From the home screen in InstrumentStudio, select **Manual Layout**.
+4) The measurement named `RF Power Meter` should appear under the *Measurements* category. Select the **Unassigned** dropdown and choose **Create large/small panel**. ![Add Measurement](/_images/AddMeasurement.png)
+
+# Current Support
+## Tested Software Versions
+- MeasurementLink 2023 Q1
+- InstrumentStudio 2023 Q1
+
+## Supported Power Meters
+| Name | Manufacturer | Software Link |
+| ---- | ------------ | ------------- |
+| NRP-Z | Rohde & Schwarz | [NRP-Z Driver](https://www.rohde-schwarz.com/driver/nrpz/) |
---
+++ RFPowerMeter/measurement.py
@@ -0,0 +1,143 @@
+"""A default measurement with an array in and out."""
+import logging
+import pathlib
+import sys
+import skrf as rf
+from skrf import network2
+
+from RsInstrument import *
+import click
+import time
+import math
+import ni_measurementlink_service as nims
+
+service_directory = pathlib.Path(__file__).resolve().parent
+measurement_service = nims.MeasurementService(
+    service_config_path=service_directory / "RFPowerMeter.serviceconfig",
+    version="0.1.0.0",
+    ui_file_paths=[service_directory / "RFPowerMeter.measui"],
+)
+
+
+@measurement_service.register_measurement
+@measurement_service.configuration("Resource Name", nims.DataType.String, "")
+@measurement_service.configuration("Frequency (Hz)", nims.DataType.Double, 1e9)
+@measurement_service.configuration("Aperture Time (s)", nims.DataType.Double, 0.01)
+@measurement_service.configuration("Auto Averaging", nims.DataType.Boolean, True)
+@measurement_service.configuration("Averaging Count", nims.DataType.Int32, 1)
+@measurement_service.configuration("Power Offset", nims.DataType.Double, 0.0)
+@measurement_service.configuration("Deembedding File Path", nims.DataType.Path, "")
+@measurement_service.output("Power (dBm)", nims.DataType.Float)
+def measure(resource_name,frequency,aperture_time,auto_averaging,averaging_count,power_offset,deembedding_path):
+
+    nrpz = None
+    # Make sure you have the last version of the RsInstrument
+    RsInstrument.assert_minimum_version('1.53.0')
+    try:
+        # -----------------------------------------------------------
+        # Initialization:
+        # -----------------------------------------------------------
+        # Adjust the VISA Resource string to fit your instrument
+        nrpz = RsInstrument(resource_name, True, False)
+        nrpz.visa_timeout = 3000  # Timeout for VISA Read Operations
+        nrpz.instrument_status_checking = True  # Error check after each command
+    except ResourceError as ex:
+        print('Error initializing the instrument session:\n' + ex.args[0])
+        exit()
+
+    print(f'Visa manufacturer: {nrpz.visa_manufacturer}')
+    print(f'Instrument Identification string: {nrpz.idn_string}')
+
+    try:
+
+        nrpz.write_str("*RST")  # Reset the instrument, clear the Error queue
+        nrpz.write_str("INIT:CONT OFF")  # Switch OFF the continuous sweep
+        # -----------------------------------------------------------
+        # Basic Settings:
+        # -----------------------------------------------------------
+        nrpz.write_str('INIT:CONT OFF')
+        nrpz.write_str('SENS:FUNC \"POW:AVG\"')
+        nrpz.write_str(f"SENS:FREQ {frequency}")
+        on_off = "ON" if auto_averaging else "OFF"
+        nrpz.write_str(f"SENS:AVER:COUNT:AUTO {on_off}")
+        nrpz.write_str(f"SENS:AVER:COUN {averaging_count}")
+        nrpz.write_str('SENS:AVER:STAT ON')
+        nrpz.write_str('SENS:AVER:TCON REP')
+
+        nrpz.write_str(f"SENS:POW:AVG:APER {aperture_time}")
+        # -----------------------------------------------------------
+        # SyncPoint 'SettingsApplied' - all the settings were applied
+        # -----------------------------------------------------------
+        nrpz.write_str("INIT:IMM")  # Start the sweep
+
+        offset = power_offset
+        if (deembedding_path != ""):
+            network = rf.Network(deembedding_path)
+            freq = rf.Frequency.from_f(frequency, unit="Hz")
+            single_matrix = network[freq]
+            offset = single_matrix.s_db[:,1,0][0]
+            print(f"using offset {offset} from s2p file")
+
+        nrpz.write_str(f"SENS:CORR:OFFS {offset}")
+        nrpz.write_str('SENS:CORR:OFFS:STAT ON')
+
+        success = False
+        for x in range(0, 200):
+            status = nrpz.query_int('STAT:OPER:COND?')
+            if (status & 16) == 0:
+                # Status register bit 4 signals MEASURING status
+                # Finished measuring, break
+                success = True
+                break
+            time.sleep(0.02)
+
+        if not success:
+            raise TimeoutError("Measurement timed out")
+
+        # -----------------------------------------------------------
+        # Fetching the results, format does not matter, the driver function always parses it correctly
+        # -----------------------------------------------------------
+        nrpz.write_str('FORMAT ASCII')
+        results = nrpz.query_str('FETCH?').split(',')
+        power_watt = float(results[0])
+        if power_watt < 0:
+            power_watt = 1E-12
+        power_dbm = 10 * math.log10(power_watt / 1E-3)
+        print(f'Measured power: {power_watt} Watt, {power_dbm:.3f} dBm')
+
+        # Close the session
+        #nrpz.close()
+
+
+        return ([power_dbm])
+    except BaseException as ex:
+        print(f"Logging exception {str(ex)}")
+        measurement_service.context.abort(-1,str(ex))
+    finally:
+        nrpz.close()
+
+
+@click.command
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Enable verbose logging. Repeat to increase verbosity.",
+)
+def main(verbose: int):
+    """Host the Sample Measurement service."""
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+
+    with measurement_service.host_service():
+        input("Press enter to close the measurement service.\n")
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
